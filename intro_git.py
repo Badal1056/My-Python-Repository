2c4422c58b5f10200f4f8bf69a83a80f51bce337
Badal183 <badaljha183@gmai.com>
2022-02-14 12:03:39 +0530

    First commit

---
+++ intro_git.py
@@ -0,0 +1,22 @@
+#what is Git?
+1. open source platform
+2. creating history of our project
+
+3. we will create a repo
+4. branching
+5. code
+6. commit: saving
+7. push
+8. merging
+9. pull
+ #initialize git in project
+ git init
+ U: untracked file
+
+ #Make my file/code ready to commit (save)
+git add -a
+file is added in the space and ready to commit
+m : Modification
+
+git commit -m 
+
