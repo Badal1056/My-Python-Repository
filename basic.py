18ccf5cb872be7aed1b626fa3b4d5eca5ab4ae60
Badal183 <badaljha183@gmai.com>
2022-02-18 13:08:51 +0530

    basicofpython

---
+++ basic.py
@@ -0,0 +1,37 @@
+#Comments: a line that is not translated by interpreter
+"""
+1.single line: #
+2.multi line: many single comment
+3.doctype/doctring: """ """
+"""
+#for identatiion: used to define a bloack
+"""
+a=2
+if a==2:
+    var="badal"
+    print("a=2")
+print (var)
+"""
+#block- defined with colon
+#block doesn't defie space of any object
+#identation at same level: block
+#statement- line of code,unit of execution
+#expession- which return something/evaluates something/ unit of evaluations
+#y=z y=z (y,z)
+
+#Token: smallest unit of program
+"""
+1.keywords
+2.identifiers
+3.literals
+4.operators:
+{
+    1.arithmetic: +, -, *, /, %
+    2.comparision: >, <, >=, <=
+    3.assignment: =, &=, not=, or=
+    4.logical: and, or, not
+    5.membership: in, notin
+    6.identity: is, notis
+    7.bitwise: leftshift, rightshift
+}
+"""
