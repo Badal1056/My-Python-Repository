320ebede7f5e4aeade67a87da68c927d09f3a8bb
Badal183 <badaljha183@gmai.com>
2022-02-18 12:48:32 +0530

    variables

--- var.py
+++ var.py
@@ -30,13 +30,84 @@ datatype variablename=data
 int var=2
 
 But int var='badal':- (error) statistically typed.
+Dynamically typed.
+Types of variable depends upon type of data stored in it.
 
+variables are case sensitive.
+var=22
+print(var)
 
+Rules to name variables:-
+1.variable name cannot start with int and symbol except _
+2.can't have white space. (simple space)
+3.Not matching any keyword:- Reserved words like print
+4.variable name is case sensitive
+"""
 
+#ways to define variablename:-
+"""
+1.camelcase: nameOfVariable
+2.snakecase: name_of_variable
+3.pascalcase: NameOfVariable
+"""
 
-
+#dynamically typed: override over variable
+"""
+var="badal"
+print(var)
+"""
+#id(): it returns memory address of any particular varaibles.
+"""
+num=22
+print(num,id(num))
+"""
+#22 is garbage and will be collected in latter.
+#variable name is simply pointer which points towards an object.
 """
 var2="badal"
 print(var2,id(var2))
 num="badal"
 print(num,id(num))
+"""
+
+#variable is constant?
+"""
+g=9.8
+print(type(g))
+"""
+#type(): return the datatype of any object
+#isinstance(): return true if specified data & type of data are same
+#syntax:
+#isinstance(object,type)
+"""
+print(isinstance(g,float))
+"""
+#everything in python is an object
+"""
+print(isinstance(g,(int,float,tuple,list)))
+class man:
+    pass
+    m1-man()
+    print(isinstance(m1,man))
+
+"""
+#can we assign single value to the multiple variables.
+"""
+yes
+a=b=c=d=30
+print(a,b,c,d)
+"""
+
+#can we assign multiple value to the multiple variables.
+"""
+yes
+a,b,c,d=1,2,3,4
+print(a,b,c,d)
+"""
+
+#Types of Variavbles:
+"""
+1.local: can be accessed inside any particular function.
+2.global: accessed anywhere in program.
+"""
+#DONE VARIABLES...
