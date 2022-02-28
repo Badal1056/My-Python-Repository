6d677a0ae7e65462976e177776b8662625066fc4
Badal183 <badaljha183@gmai.com>
2022-02-28 11:50:37 +0530

    if-else

---
+++ ifelse.py
@@ -0,0 +1,16 @@
+#ifelse: A statement which helps in decision making.
+"""
+person='badal'
+mode='black'
+if mode=='black' #Purpose: check particular situation.
+    #block. will execute
+print('{} will reach to college in 15 minutes'.format(person))
+else:
+    print('Depends upon the other mode.')
+"""
+person='badal'
+color='black'
+if color=='black':
+    print('My favourite color'.format(person))
+else:
+    print('Not my favourite color')
